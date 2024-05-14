e9c8c818ef8d47d0d35a2260752b488b1c82aa86
Melanie-J-Baker <baker_mel@hotmail.com>
2024-05-14 15:02:58 +0100

    Import character image files in Characters component

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,4 +1,8 @@
 import PropTypes from 'prop-types';
+import Wally from '../images/wally.jpeg';
+import Wendy from '../images/wendy.jpeg';
+import Oddlaw from '../images/oddlaw.jpeg';
+import Wizard from '../images/wizard.jpeg';
 
 Characters.propTypes = {
     charData: PropTypes.array,
@@ -12,7 +16,7 @@ function Characters({ charData }) {
                 {' '}
                 {charData.map((char) => (
                     <div key={char._id} className="char">
-                        <img className='charKeyImg' src={char.imgUrl} alt='charImg' />
+                        <img className='charKeyImg' src={char.name} alt='charImg' />
                         <h1 className='charKeyName'>{char.name}</h1>
                     </div>
                 ))}
