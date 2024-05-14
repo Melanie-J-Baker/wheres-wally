a5193b4076481cbdc632f08d64a2b9e0367bd684
Melanie-J-Baker <baker_mel@hotmail.com>
2024-05-14 15:27:12 +0100

    Change image imports in Characters component

--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,8 +1,8 @@
 import PropTypes from 'prop-types';
-import Wally from '../images/wally.jpeg';
-import Wendy from '../images/wendy.jpeg';
-import Oddlaw from '../images/oddlaw.jpeg';
-import Wizard from '../images/wizard.jpeg';
+import '../images/wally.jpeg';
+import '../images/wendy.jpeg';
+import '../images/oddlaw.jpeg';
+import '../images/wizard.jpeg';
 
 Characters.propTypes = {
     charData: PropTypes.array,
@@ -16,7 +16,7 @@ function Characters({ charData }) {
                 {' '}
                 {charData.map((char) => (
                     <div key={char._id} className="char">
-                        <img className='charKeyImg' src={char.name} alt='charImg' />
+                        <img className='charKeyImg' src={char.imgUrl} alt='charImg' />
                         <h1 className='charKeyName'>{char.name}</h1>
                     </div>
                 ))}
